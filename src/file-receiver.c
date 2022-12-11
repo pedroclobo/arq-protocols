d61f529b7e9288fa22d53b49c5efc33e8a89ac51
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-12-11 23:00:01 +0000

    Implement stop-and-wait

--- src/file-receiver.c
+++ src/file-receiver.c
@@ -24,30 +24,66 @@ int find_last_path_separator(char *path) {
 	return last_found_pos;
 }
 
+ssize_t send_ack_packet(int sockfd, struct ack_pkt_t *ack_pkt, size_t len, struct sockaddr *src_addr) {
+	size_t sent_len = sendto(sockfd, ack_pkt, len, 0, src_addr, sizeof(*src_addr));
+
+	if (sent_len != len) {
+		fprintf(stderr, "Truncated packet.\n");
+		exit(-1);
+	}
+
+	printf("Receiver: Sent ACK segment %d, size %ld.\n", ntohl(ack_pkt->seq_num), sent_len);
+
+	return sent_len;
+}
+
+ssize_t receive_data_packet(int sockfd, struct data_pkt_t *data_pkt, struct sockaddr *src_addr, socklen_t *src_addr_len) {
+	ssize_t received_len = recvfrom(sockfd, data_pkt, sizeof(*data_pkt), 0, src_addr, src_addr_len);
+
+	if (received_len == -1) {
+		return -1;
+	} else if (received_len == 0) {
+		return 0;
+	}
+
+	printf("Receiver: Received segment %d, size %ld.\n", ntohl(data_pkt->seq_num), received_len);
+
+	return received_len;
+}
+
 int main(int argc, char *argv[]) {
+	// Parse command line arguments.
+	if (argc != 5) {
+		fprintf(stderr, "Usage: %s <file> <host> <port> <window-size>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
 	char *file_path = argv[1];
 	char *host = argv[2];
 	int port = atoi(argv[3]);
 
+	int window_size = atoi(argv[4]);
+	if (window_size < 1 || window_size > MAX_WINDOW_SIZE) {
+		fprintf(stderr, "Invalid window size\n");
+		exit(EXIT_FAILURE);
+	}
+
 	int last_path_sep_index = find_last_path_separator(file_path);
 	char *file_name = file_path;
 
-	if (last_path_sep_index != -1 &&
-	    last_path_sep_index < MAX_PATH_SIZE - 1) {
+	if (last_path_sep_index != -1 && last_path_sep_index < MAX_PATH_SIZE - 1) {
 		file_name = file_path + last_path_sep_index + 1;
 	}
 
 	FILE *file = fopen(file_name, "w");
 	if (!file) {
-		perror("fopen");
-		exit(-1);
+		exit(EXIT_FAILURE);
 	}
 
 	// Prepare server host address.
 	struct hostent *he;
 	if (!(he = gethostbyname(host))) {
-		perror("gethostbyname");
-		exit(-1);
+		exit(EXIT_FAILURE);
 	}
 
 	struct sockaddr_in srv_addr = {
@@ -58,40 +94,66 @@ int main(int argc, char *argv[]) {
 
 	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sockfd == -1) {
-		perror("socket");
-		exit(-1);
+		exit(EXIT_FAILURE);
 	}
 
+	// Request file.
 	req_file_pkt_t req_file_pkt;
 	size_t file_path_len = strlen(file_path);
 	strncpy(req_file_pkt.file_path, file_path, file_path_len);
 
-	ssize_t sent_len =
-	    sendto(sockfd, &req_file_pkt, file_path_len, 0,
-	           (struct sockaddr *)&srv_addr, sizeof(srv_addr));
+	ssize_t sent_len = sendto(sockfd, &req_file_pkt, file_path_len, 0, (struct sockaddr *)&srv_addr, sizeof(srv_addr));
 	if (sent_len != file_path_len) {
 		fprintf(stderr, "Truncated packet.\n");
-		exit(-1);
+		exit(EXIT_FAILURE);
 	}
-	printf("Sending request for file %s, size %ld.\n", file_path,
-	       file_path_len);
+	printf("Sending request for file %s, size %ld.\n", file_path, file_path_len);
 
-	ssize_t len;
-	do { // Iterate over segments, until last the segment is detected.
-		// Receive segment.
-		data_pkt_t data_pkt;
-		struct sockaddr_in src_addr;
+	// Set timeout.
+	struct timeval tv;
+	tv.tv_sec = 4;
+	tv.tv_usec = 0;
+	if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) == -1) {
+		fprintf(stderr, "Failed to set timeout\n");
+		exit(EXIT_FAILURE);
+	}
 
-		len = recvfrom(sockfd, &data_pkt, sizeof(data_pkt), 0,
-		               (struct sockaddr *)&src_addr,
-		               &(socklen_t){sizeof(src_addr)});
-		printf("Received segment %d, size %ld.\n",
-		       ntohl(data_pkt.seq_num), len);
+	uint32_t seq_num = 0;
 
-		// Write data to file.
-		fwrite(data_pkt.data, 1, len - offsetof(data_pkt_t, data),
-		       file);
-	} while (len == sizeof(data_pkt_t));
+	ssize_t received_len;
+	data_pkt_t data_pkt;
+	ack_pkt_t ack_pkt;
+	struct sockaddr_in src_addr;
+
+	// Iterate over segments, until last the segment is detected.
+	do {
+		// Receive data packet.
+		received_len = receive_data_packet(sockfd, &data_pkt, (struct sockaddr *)&src_addr, &(socklen_t){sizeof(src_addr)});
+		if (received_len == -1) {
+			fprintf(stderr, "Receiver timeout has been reached\n");
+			fclose(file);
+			exit(EXIT_FAILURE);
+		}
+
+		// Corrupted packet
+		/* if (received_len != sizeof(data_pkt_t)) {
+		        continue;
+		} */
+
+		// Write new packet data to file.
+		// If seq_num != data_pkt.seq_num the packet is duplicated and we need to
+		// send an ACK
+		if (seq_num == ntohl(data_pkt.seq_num)) {
+			fwrite(data_pkt.data, 1, received_len - offsetof(data_pkt_t, data), file);
+			seq_num = (seq_num + 1) % MAX_WINDOW_SIZE;
+		}
+
+		// Send ACK
+		ack_pkt.seq_num = htonl(seq_num);
+		ack_pkt.selective_acks = 0; // TODO
+		sent_len = send_ack_packet(sockfd, &ack_pkt, sizeof(ack_pkt), (struct sockaddr *)&src_addr);
+
+	} while (received_len == sizeof(data_pkt_t));
 
 	// Clean up and exit.
 	close(sockfd);
--- src/file-sender.c
+++ src/file-sender.c
@@ -10,22 +10,64 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+ssize_t send_data_packet(int sockfd, struct data_pkt_t *data_pkt, size_t len, struct sockaddr *src_addr) {
+	size_t sent_len = sendto(sockfd, data_pkt, len, 0, src_addr, sizeof(*src_addr));
+	printf("Sender: Sending segment %d, size %ld.\n", ntohl(data_pkt->seq_num), len);
+
+	if (sent_len != len) {
+		fprintf(stderr, "Truncated packet.\n");
+		exit(-1);
+	}
+
+	return sent_len;
+}
+
+ssize_t receive_ack_packet(int sockfd, struct ack_pkt_t *ack_pkt, struct sockaddr *src_addr, socklen_t *src_addr_len) {
+	ssize_t received_len = recvfrom(sockfd, ack_pkt, sizeof(*ack_pkt), 0, src_addr, src_addr_len);
+
+	if (received_len == -1) {
+		return -1;
+	} else if (received_len == 0) {
+		return 0;
+	}
+
+	if (received_len != sizeof(*ack_pkt)) {
+		fprintf(stderr, "Truncated packet.\n");
+		exit(-1);
+	}
+
+	printf("Sender: Received ACK segment %d, size %ld.\n", ntohl(ack_pkt->seq_num), received_len);
+
+	return received_len;
+}
+
 int main(int argc, char *argv[]) {
+	// Parse command line arguments.
+	if (argc != 3) {
+		fprintf(stderr, "Usage: %s <port> <window-size>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
 	int port = atoi(argv[1]);
+
+	int window_size = atoi(argv[2]);
+	if (window_size <= 0 || window_size > MAX_WINDOW_SIZE) {
+		fprintf(stderr, "Invalid window size\n");
+		exit(EXIT_FAILURE);
+	}
 
 	// Prepare server socket.
 	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sockfd == -1) {
-		perror("socket");
-		exit(-1);
+		fprintf(stderr, "Failed to prepare server socket\n");
+		exit(EXIT_FAILURE);
 	}
 
 	// Allow address reuse so we can rebind to the same port,
 	// after restarting the server.
-	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){1},
-	               sizeof(int)) < 0) {
-		perror("setsockopt");
-		exit(-1);
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0) {
+		fprintf(stderr, "Failed to allow address reuse\n");
+		exit(EXIT_FAILURE);
 	}
 
 	struct sockaddr_in srv_addr = {
@@ -34,8 +76,7 @@ int main(int argc, char *argv[]) {
 	    .sin_port = htons(port),
 	};
 	if (bind(sockfd, (struct sockaddr *)&srv_addr, sizeof(srv_addr))) {
-		perror("bind");
-		exit(-1);
+		exit(EXIT_FAILURE);
 	}
 	fprintf(stderr, "Receiving on port: %d\n", port);
 
@@ -43,43 +84,52 @@ int main(int argc, char *argv[]) {
 	struct sockaddr_in src_addr;
 	req_file_pkt_t req_file_pkt;
 
-	len = recvfrom(sockfd, &req_file_pkt, sizeof(req_file_pkt), 0,
-	               (struct sockaddr *)&src_addr,
-	               &(socklen_t){sizeof(src_addr)});
+	// Receive name of file from client.
+	len = recvfrom(sockfd, &req_file_pkt, sizeof(req_file_pkt), 0, (struct sockaddr *)&src_addr, &(socklen_t){sizeof(src_addr)});
 	if (len < MAX_PATH_SIZE) {
 		req_file_pkt.file_path[len] = '\0';
 	}
-	printf("Received request for file %s, size %ld.\n",
-	       req_file_pkt.file_path, len);
+	printf("Received request for file %s, size %ld.\n", req_file_pkt.file_path, len);
 
 	FILE *file = fopen(req_file_pkt.file_path, "r");
 	if (!file) {
-		perror("fopen");
-		exit(-1);
+		exit(EXIT_FAILURE);
+	}
+
+	// Set timeout.
+	struct timeval tv;
+	tv.tv_sec = TIMEOUT / 1000;
+	tv.tv_usec = (TIMEOUT % 1000) * 1000;
+	if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) == -1) {
+		fprintf(stderr, "Failed to set timeout\n");
+		exit(EXIT_FAILURE);
 	}
 
 	uint32_t seq_num = 0;
+
 	data_pkt_t data_pkt;
+	ack_pkt_t ack_pkt;
 	size_t data_len;
-	do { // Generate segments from file, until the the end of the file.
-		// Prepare data segment.
-		data_pkt.seq_num = htonl(seq_num++);
+	size_t received_len;
 
-		// Load data from file.
+	// Generate segments from file, until the end of the file.
+	do {
+		// Send data segment.
+		data_pkt.seq_num = htonl(seq_num);
 		data_len = fread(data_pkt.data, 1, sizeof(data_pkt.data), file);
-		printf("read %ld\n", data_len);
+		send_data_packet(sockfd, &data_pkt, offsetof(data_pkt_t, data) + data_len, (struct sockaddr *)&src_addr);
 
-		// Send segment.
-		ssize_t sent_len = sendto(
-		    sockfd, &data_pkt, offsetof(data_pkt_t, data) + data_len, 0,
-		    (struct sockaddr *)&src_addr, sizeof(src_addr));
-		printf("Sending segment %d, size %ld.\n",
-		       ntohl(data_pkt.seq_num),
-		       offsetof(data_pkt_t, data) + data_len);
-		if (sent_len != offsetof(data_pkt_t, data) + data_len) {
-			fprintf(stderr, "Truncated packet.\n");
-			exit(-1);
-		}
+		// Wait for ACK and resend if timeout.
+		do {
+			received_len = receive_ack_packet(sockfd, &ack_pkt, (struct sockaddr *)&src_addr, &(socklen_t){sizeof(src_addr)});
+			if (received_len == -1) {
+				fprintf(stderr, "Timeout, resending segment %d\n", ntohl(data_pkt.seq_num));
+				send_data_packet(sockfd, &data_pkt, offsetof(data_pkt_t, data) + data_len, (struct sockaddr *)&src_addr);
+			}
+		} while (received_len == -1 || ntohl(ack_pkt.seq_num) != (seq_num + 1) % MAX_WINDOW_SIZE);
+
+		seq_num = (seq_num + 1) % MAX_WINDOW_SIZE;
+
 	} while (!(feof(file) && data_len < sizeof(data_pkt.data)));
 
 	// Clean up and exit.
